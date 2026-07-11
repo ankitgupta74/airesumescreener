02fb146cd44c5e1f1a23bcf74c68536a0330902b
Ankit Gupta <ankitgupta.connectmail@gmail.com>
2026-07-11 11:52:28 +0530

    Authentication

--- client/aiClient.ts
+++
@@ -1,7 +0,0 @@
-import { GoogleGenAI } from "@google/genai";
-import { configDotenv } from "dotenv";
-
-configDotenv();
-
-// Automatically reads process.env.GEMINI_API_KEY
-export const aiClient = new GoogleGenAI({});
